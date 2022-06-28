aaebd7b35b1c17a438876b26608f86c70eb32a52
joyfulpebble <danmax32100@gmail.com>
2022-06-28 21:37:01 +0300

    отображение списка методом render()

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -4,15 +4,30 @@ declare global {
   }
 }
 
+const template = `
+  <div class="select__input">
+    <span class="input__text"> </span>
+    <i class="fa-solid fa-angle-down fa-rotate-270" id="angle"></i>
+  </div>
+  <div class="select__dropdown">
+    <ul class="select__list">
+      <li class="select__item"> text </li>
+    </ul>
+  </div>
+`
+
 export class Select {
   $selector: any;
   $angle: HTMLElement | null;
 
   constructor(selector: string, options: options){
     this.$selector = document.getElementById(selector);
-    this.$angle = document.getElementById(options.angle)
+    this.$angle = document.getElementById(options.angle);
     
-
+    this.render();
+  }
+  private render(){
+    this.$selector.innerHTML = template;
   }
 
   open(){
@@ -23,5 +38,6 @@ export class Select {
     this.$selector.classList.remove('active');
     this.$angle?.classList.add('fa-rotate-270');
   }
+
 
 }
